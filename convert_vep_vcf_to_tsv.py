11d94870671c9950dca80cc33019aa68d9b546e4
Ann Black-Ziegelbein <elizabeth-black@uiowa.edu>
2016-08-30 21:47:30 -0500

    MORL-447: fixing the split and re-splice of the line around the VEP region

--- convert_vep_vcf_to_tsv.py
+++ convert_vep_vcf_to_tsv.py
@@ -96,18 +96,20 @@ def process_variant(
         Reformatting line such that the '|' -> '\t' replacement is ONLY done on VEP section
         Other info fields could use the '|' delimiter
     """
-    vep_index = line.index(info_key)
-    if (vep_index >= 0):
-        line_before_VEP = line[0: vep_index]
-        line_VEP_to_end = line[vep_index: len(line)]
-        try:
-            vep_end_index = line_VEP_to_end.index(';')
-        except ValueError:
-            vep_end_index = line_VEP_to_end.index('\t')
-        line_VEP_part = line[vep_index: vep_end_index]
-        line_after_VEP = line[vep_end_index: len(line)]
-        line = line_before_VEP + line_VEP_part.replace('|', '\t') + line_after_VEP
-
+    try:
+        vep_index = line.index(info_key)
+        if (vep_index >= 0):
+            line_before_VEP = line[0: vep_index]
+            line_VEP_to_end = line[vep_index: len(line)]
+            try:
+                vep_end_index = line_VEP_to_end.index(';')
+            except ValueError:
+                vep_end_index = line_VEP_to_end.index('\t')
+            line_VEP_part = line_VEP_to_end[0: vep_end_index]
+            line_after_VEP = line_VEP_to_end[vep_end_index: len(line_VEP_to_end)]
+            line = line_before_VEP + line_VEP_part.replace('|', '\t') + line_after_VEP
+    except ValueError:
+        pass
     line = line.strip().split('\t')
     is_vep_info_col = [info_key in z for z in line]
     if True not in is_vep_info_col:
